f778642db258bd245bff8afbe83abc71eb443003
Virginia <virginia_hol@hotmail.com>
2021-02-20 11:16:02 -0300

    alterecao02

--- Program.cs
+++ Program.cs
@@ -4,15 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Aula
+namespace Calc
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //simples calculadora
+            
 
-            double num1, num2;//variáveis que irão conter os números digitados
+            double num1, num2;
             string tmp;
 
             Console.WriteLine("Digite um número: ");
@@ -25,19 +25,19 @@ namespace Aula
 
             double resultado;
 
-            //realiza a soma e imprime no console
+            
             resultado = num1 + num2;
             Console.WriteLine("Soma: " + resultado);
 
-            //realiza a subtração e imprime no console
+            
             resultado = num1 - num2;
             Console.WriteLine("Subtração: " + resultado);
 
-            //realiza a multiplicação e imprime no console
+            
             resultado = num1 * num2;
             Console.WriteLine("Multiplicação: " + resultado);
 
-            //realiza a divisão e imprime no console
+            
             resultado = num1 / num2;
             Console.WriteLine("Divisão: " + resultado);
         }
